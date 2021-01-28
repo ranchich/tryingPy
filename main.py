4be8bfdca4ec53f82152168b9d3e08721bbc6c62
Ran Chicheportiche <ran.shish@gmail.com>
2021-01-28 20:27:13 +0200

    seconed commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("hello world")
+list = [
+    {"name": "ran", "city": "jerusalem"},
+    {"name": "gil", "city": "tel aviv"},
+    {"name": "yuval", "city": "givaataim"}
+]
+
+def f (person):
+    return person ["city"]
+
+list.sort(key=f)
+
+print(list)
